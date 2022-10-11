cfba78e7bea4053d049ab7f2ac88f38f971e4df0
David Kirby <david@kinetic.com>
2022-10-11 15:52:46 -0500

    added 404.php

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div class="content">
+
+<div class="main">
+
+<h1>404</h1>
+
+<h2 class="message">Error: Page not found.</h2>
+
+</div>
+
+</div>
+
+<?php get_footer(); ?>
